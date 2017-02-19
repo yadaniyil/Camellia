bc98e1cfda919228693f42e2a76d863440a6d478
Daniil <danilyakovlev2009@gmail.com>
2017-02-19 23:48:54 +0200

    Added calculation support keys

--- src/Camellia.java
+++ src/Camellia.java
@@ -30,6 +30,7 @@ public class Camellia {
     public Camellia() {
         createKeys();
         generateSupportKeys();
+        calculateSupportKeys();
     }
 
     private void createKeys() {
@@ -52,5 +53,26 @@ public class Camellia {
         d2 = d2 ^ Functions.F(d1, c5);
         d1 = d1 ^ Functions.F(d2, c6);
         kb = (d1 << 64) | d2;
+    }
+
+    private void calculateSupportKeys() {
+        long kw1, kw2, kw3, kw4;
+        long k1, k2, k3, k4, k5, k6, k7, k8, k9,
+                k10, k11, k12, k13, k14, k15, k16, k17, k18;
+        long ke1, ke2, ke3, ke4;
+        kw1 = (kl) >> 64;
+        kw2 = (kl) & MASK64;
+        k1  = (ka) >> 64; k2  = (ka & MASK64);
+        k3  = (kl <<  15) >> 64; k4  = (kl <<  15) & MASK64;
+        k5  = (ka <<  15) >> 64; k6  = (ka <<  15) & MASK64;
+        ke1 = (ka <<  30) >> 64; ke2 = (ka <<  30) & MASK64;
+        k7  = (kl <<  45) >> 64; k8  = (kl <<  45) & MASK64;
+        k9  = (ka <<  45) >> 64; k10 = (kl <<  60) & MASK64;
+        k11 = (ka <<  60) >> 64; k12 = (ka <<  60) & MASK64;
+        ke3 = (kl <<  77) >> 64; ke4 = (kl <<  77) & MASK64;
+        k13 = (kl <<  94) >> 64; k14 = (kl <<  94) & MASK64;
+        k15 = (ka <<  94) >> 64; k16 = (ka <<  94) & MASK64;
+        k17 = (kl << 111) >> 64; k18 = (kl << 111) & MASK64;
+        kw3 = (ka << 111) >> 64;kw4 = (ka << 111) & MASK64;
     }
 }
